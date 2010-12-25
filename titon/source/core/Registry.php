bd4564e64726e347b76453d8b01d445e1f557fa8
Miles Johnson <mileswjohnson@gmail.com>
2010-12-24 21:32:45 -0800

    Rewriting the Registry class. Making it more light weight and removing unneeded features.

--- titon/source/core/Registry.php
+++ titon/source/core/Registry.php
@@ -1,156 +1,114 @@
 <?php
 /**
- * The Registry acts a central hub where any part of the application can access a single instance of a stored object.
- * It registers all objects and instances into the class to store in memory and be re-useable later at any given time.
- * Is also packaged to handle registry on a per module basis using the architecture setup.
+ * Titon: The PHP 5.3 Micro Framework
  *
- * @copyright	Copyright 2009, Titon (A PHP Micro Framework)
- * @link		http://titonphp.com
- * @license		http://opensource.org/licenses/bsd-license.php (The BSD License)
+ * @copyright	Copyright 2010, Titon
+ * @link		http://github.com/titon
+ * @license		http://opensource.org/licenses/bsd-license.php (BSD License)
  */
 
-namespace titon\core;
+namespace titon\source\core;
 
-use \titon\core\App;
 use \titon\log\Exception;
-use \titon\utility\Set;
 
 /**
- * Registry Class
+ * The Registry acts a central hub where any part of the application can access a single instance of a stored object.
+ * It registers all objects into the class to store in memory and be re-useable later at any given time.
  *
- * @package		Titon
- * @subpackage	Titon.Core
+ * @package	titon.source.core
+ * @uses	Exception
  */
 class Registry {
 
-    /**
-     * Return the listing as literal objects. Argument setting for listObjects().
-     *
-     * @var boolean
-     */
-    const LIST_OBJECTS = 1;
-
-    /**
-     * Return the listing by class names. Argument setting for listObjects().
-     *
-     * @var boolean
-     */
-    const LIST_NAMES = 2;
-
-    /**
-     * Configuration settings that are automatically loaded into classes upon instantiation.
-     *
-     * @access private
-     * @var array
-     * @static
-     */
-    private static $__config = array();
-
 	/**
-	 * Class names of all registered objects; grouped by their dot path or namespace architecture.
+	 * Configuration settings that are automatically loaded into classes upon instantiation.
 	 *
 	 * @access private
 	 * @var array
-	 * @static
 	 */
-	private static $__mapping = array();
+	private $__configs = array();
 
 	/**
 	 * Objects that have been registered into memory. The array index is represented by the namespace convention,
-     * where as the array value would be the matching instantiated object.
+	 * where as the array value would be the matching instantiated object.
 	 *
 	 * @access private
 	 * @var array
-	 * @static
 	 */
-	private static $__registered = array();
+	private $__registered = array();
 
-	/**
-	 * Disable the class to enforce static methods.
-	 *
-	 * @access private
-	 * @return void
-	 */
-	private function __construct() { }
-	
 	/**
 	 * Checks to see if an object has been registered (instantiated).
 	 *
 	 * @access public
-	 * @param string $slug
+	 * @param string $key
 	 * @return boolean
-	 * @static
 	 */
-	public static function check($slug) {
-		return (isset(static::$__registered[$slug]) && is_object(static::$__registered[$slug]));
+	public function check($key) {
+		return (isset($this->__registered[$key]) && is_object($this->__registered[$key]));
 	}
 
-    /**
-     * Defines an array of configuration that should be loaded into a class when its instantiated.
-     *
-     * @access public
-     * @param string $slug
-     * @param array $config
-     * @return void
-     * @static
-     */
-    public static function configure($slug, array $config = array()) {
-        if (isset(static::$__config[$slug])) {
-            static::$__config[$slug] = $config + static::$__config[$namespace];
-        } else {
-            static::$__config[$slug] = $config;
-        }
-    }
-	
 	/**
-	 * Delete an object from registry and remove its name from the mapping.
+	 * Defines an array of configuration that should be loaded into a class when its instantiated.
+	 *
+	 * @access public
+	 * @param string $key
+	 * @param array $config
+	 * @return void
+	 */
+	public function configure($key, array $config = array()) {
+		if (isset($this->__configs[$key])) {
+			$this->__configs[$key] = $config + $this->__configs[$key];
+		} else {
+			$this->__configs[$key] = $config;
+		}
+	}
+
+	/**
+	 * Delete an object from registry.
 	 * Returns true if object exists and was deleted, else returns false.
 	 *
 	 * @access public
-	 * @param string $slug
+	 * @param string $key
 	 * @return boolean
-	 * @static
 	 */
-	public static function delete($slug) {
-		if (isset(static::$__registered[$slug])) {
-			unset(static::$__registered[$slug]);
-
-			static::$__mapping = Set::remove(static::$__mapping, $slug);
+	public function delete($key) {
+		if (isset($this->__registered[$key])) {
+			unset($this->__registered[$key]);
 			return true;
 		}
-		
+
 		return false;
 	}
 
 	/**
-	 * Register a file into memory and return an instantiated object, based on a dot notated path.
+	 * Register a file into memory and return an instantiated object.
 	 *
 	 * @access public
-	 * @param string $slug
+	 * @param string $key
 	 * @param array $config
 	 * @return object
-	 * @static
 	 */
-	public static function factory($slug, array $config = array()) {
-        if (isset(static::$__registered[$slug])) {
-            return static::$__registered[$slug];
-        }
+	public function factory($key, array $config = array()) {
+		if (isset($this->__registered[$key])) {
+			return $this->__registered[$key];
+		}
 
-        $namespace = App::toNamespace($slug);
+		$namespace = $app->loader->toNamespace($key);
 
-        if (!class_exists($namespace)) {
-            App::import($namespace);
-        }
+		if (!class_exists($namespace)) {
+			$app->loader->import($key);
+		}
 
-        if (class_exists($namespace)) {
-            if (isset(static::$__config[$slug])) {
-                $config = $config + static::$__config[$slug];
-            }
+		if (class_exists($namespace)) {
+			if (isset($this->__configs[$key])) {
+				$config = $config + $this->__configs[$key];
+			}
 
-            return static::store(new $namespace($config));
-        } else {
-            throw new Exception(sprintf('Class "%s" could not be instantiated into the registry.', $slug));
-        }
+			return $this->store(new $namespace($config));
+		} else {
+			throw new Exception(sprintf('Class "%s" could not be instantiated into the registry.', $key));
+		}
 	}
 
 	/**
@@ -158,72 +116,45 @@ class Registry {
 	 *
 	 * @access public
 	 * @return void
-	 * @static
 	 */
-	public static function flush() {
-		static::$__registered = array();
-		static::$__mapping = array();
-        static::$__config = array();
-	}
-	
-	/**
-	 * Returns an array of all objects that have been registered, valued by their dot notation.
-	 *
-	 * @access public
-	 * @param boolean $showObject
-	 * @return array
-	 * @static
-	 */
-	public static function listObjects($mode = self::LIST_NAMES) {
-		if (empty(static::$__registered)) {
-			return static::$__registered;
-		} else {
-            switch ($mode) {
-                case self::LIST_OBJECTS:    return static::$__registered; break;
-                case self::LIST_NAMES:      return array_keys(static::$__registered); break;
-            }
+	public function flush() {
+		$this->__configs = array();
+
+		foreach ($this->__registered as $key => $object) {
+			unset($this->__registered[$key]);
 		}
 	}
 
 	/**
-	 * Return a full detailed list of all registered objects organized by their slug.
-	 * Can filter down the architecture by supplying a dot path notation.
+	 * Returns an array of all objects that have been registered; returns the keys and not the objects.
 	 *
 	 * @access public
-	 * @param string $path
 	 * @return array
-	 * @static
 	 */
-	public static function listMapping($path = null) {
-        return empty($path) ? static::$__mapping : Set::extract(static::$__mapping, (string)$path);
+	public function listing() {
+		return array_keys($this->__registered);
 	}
 
 	/**
-	 * Manually store an object into registry, and store the object name to the $__mapping array.
-	 * The property $__registered contains the object where as $__mapping is just the class name.
+	 * Store an object into registry.
 	 *
 	 * @access public
 	 * @param object $object
-     * @param string $slug
+	 * @param string $key
 	 * @return object
-	 * @static
 	 */
-	public static function store($object, $slug = null) {
+	public function store($object, $key = null) {
 		if (!is_object($object)) {
 			throw new Exception('The object passed must be instantiated.');
 		}
 
-		$namespace = get_class($object);
-        $className = App::baseClass($namespace);
+		if (!$key) {
+			$key = $app->loader->toNotation(get_class($object));
+		}
 
-        if (!$slug) {
-            $slug = App::toDotNotation($namespace);
-        }
-
-		static::$__registered[$slug] = $object;
-		static::$__mapping = Set::insert(static::$__mapping, $slug, $className);
+		$this->__registered[$key] = $object;
 
 		return $object;
 	}
-	
+
 }
