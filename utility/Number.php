bc24343c8b05762788b4953c2602e3bac0322711
Miles Johnson <mileswjohnson@gmail.com>
2012-07-19 16:06:05 -0700

    Added base constants for titon\utility\Number Removed useless methods and finished incomplete methods

--- utility/Number.php
+++ utility/Number.php
@@ -9,24 +9,80 @@
 
 namespace titon\utility;
 
+use titon\Titon;
+
 /**
- * The Number utility allows for the twiddling and calculation of numbers (non-floats) and provides support for generic integer bases.
- * Provides helper methods to easy in the evaluation of numbers within context.
+ * The Number utility allows for the twiddling and calculation of numbers and floats.
+ * Provides helper methods to ease in the evaluation of numbers within context.
  *
  * @package	titon.utility
  */
 class Number {
 
 	/**
-	 * Convert a word notated form of bytes (1MB) to the numerical equivalent.
+	 * Bases.
+	 */
+	const BINARY = 2;
+	const OCTAL = 8;
+	const DECIMAL = 10;
+	const HEX = 16;
+
+	/**
+	 * Convert a readable string notated form of bytes (1KB) to the numerical equivalent (1024).
+	 * Supports all the different format variations: k, kb, ki, kib, etc.
 	 *
 	 * @access public
 	 * @param int $number
 	 * @return int
 	 * @static
 	 */
-	public static function bytes($number) {
-		// @todo
+	public static function bytesFrom($number) {
+		$number = trim((string) $number);
+		$sizes = [
+			'k|kb|ki|kib' => 10,
+			'm|mb|mi|mib' => 20,
+			'g|gb|gi|gib' => 30,
+			't|tb|ti|tib' => 40,
+			'p|pb|pi|pib' => 50,
+			'e|eb|ei|eib' => 60,
+			'z|zb|zi|zib' => 70,
+			'y|yb|yi|yib' => 80,
+			'b' => 0
+		];
+
+		foreach ($sizes as $format => $pow) {
+			if (preg_match('/^([0-9\.]+)(' . $format . ')?$/i', $number, $matches)) {
+				$size = (float) $matches[1];
+
+				if (empty($matches[2])) {
+					return $size;
+				}
+
+				return ($size * pow(2, $pow));
+			}
+		}
+
+		return $number;
+	}
+
+	/**
+	 * Convert a numerical value to the readable string notated equivalent.
+	 *
+	 * @access public
+	 * @param int $size
+	 * @param int $precision
+	 * @return string
+	 * @static
+	 */
+	public static function bytesTo($size, $precision = 0) {
+		$sizes = ['YB', 'ZB', 'EB', 'PB', 'TB', 'GB', 'MB', 'KB', 'B'];
+		$total = count($sizes);
+
+		while ($total-- && $size >= 1024) {
+			$size /= 1024;
+		}
+
+		return self::precision($size, $precision) . $sizes[$total];
 	}
 
 	/**
@@ -40,40 +96,98 @@ class Number {
 	 * @static
 	 */
 	public static function convert($no, $fromBase, $toBase) {
+		if ($fromBase == $toBase) {
+			return $no;
+		}
+
 		return base_convert($no, $fromBase, $toBase);
 	}
 
 	/**
 	 * Convert a number to it's currency equivalent, respecting locale.
+	 * Allow for overrides through an options array.
 	 *
 	 * @access public
 	 * @param int $number
-	 * @return int
+	 * @param array $options
+	 * @return string
 	 * @static
 	 */
-	public static function currency($number) {
-		// @todo
-	}
+	public static function currency($number, array $options = []) {
+		$defaults = [
+			'thousands' => ',',
+			'decimals' => '.',
+			'places' => 2,
+			'code' => 'USD #',
+			'dollar' => '$#',
+			'cents' => '#&cent;',
+			'use' => 'dollar',
+			'negative' => '(#)'
+		];
 
-	public static function format() {}
+		// Localization support
+		if (Titon::g11n()->isEnabled()) {
+			$defaults = array_merge($defaults,
+				Titon::g11n()->current()->getFormats('number'),
+				Titon::g11n()->current()->getFormats('currency'));
+		}
 
-	/**
-	 * Return the highest number out of all the arguments. If no arguments are passed, -1 will be returned.
-	 *
-	 * @access public
-	 * @return int
-	 * @static
-	 */
-	public static function high() {
-		$high = -1;
+		$options = $options + $defaults;
+		$amount = number_format(self::precision(abs($number), $options['places']), $options['places'], $options['decimals'], $options['thousands']);
 
-		foreach (func_get_args() as $number) {
-			if ($number > $high) {
-				$high = $number;
+		// Cents
+		if (($number < 1 && $number > -1) && $options['cents']) {
+			$amount = str_replace('#', $amount, $options['cents']);
+
+		// Dollars
+		} else {
+			if ($options['use'] === 'dollar') {
+				$amount = str_replace('#', $amount, $options['dollar']);
+			} else {
+				$amount = str_replace('#', $amount, $options['code']);
 			}
 		}
 
-		return $high;
+		// Negative
+		if ($number < 0 && $options['negative']) {
+			$amount = str_replace('#', $amount, $options['negative']);
+		}
+
+		return $amount;
+	}
+
+	/**
+	 * Format a number to a certain string sequence. All #'s in the format will be replaced by the number in the same position within the sequence.
+	 * All *'s will mask the number in the sequence. Large numbers should be passed as strings.
+	 *
+	 * {{{
+	 * 		Number::format(1234567890, '(###) ###-####');				(123) 456-7890
+	 * 		Number::format(1234567890123456, '****-****-####-####');	****-****-9012-3456
+	 * }}}
+	 *
+	 * @access public
+	 * @param int|string $number
+	 * @param string $format
+	 * @return mixed
+	 * @static
+	 */
+	public static function format($number, $format) {
+		$number = (string) $number;
+		$length = strlen($format);
+		$result = $format;
+		$pos = 0;
+
+		for ($i = 0; $i < $length; $i++) {
+			$char = $format[$i];
+
+			if (($char === '#' || $char === '*') && isset($number[$pos])) {
+				$replace = ($char === '*') ? '*' : $number[$pos];
+				$result = substr_replace($result, $replace, $i, 1);
+				$pos++;
+			}
+		}
+
+		return $result;
 	}
 
 	/**
@@ -154,26 +268,6 @@ class Number {
 	}
 
 	/**
-	 * Return the lowest number out of all the arguments. If no arguments are passed, -1 will be returned.
-	 *
-	 * @access public
-	 * @return int
-	 * @static
-	 */
-	public static function low() {
-		$high = call_user_func_array(array(__CLASS__, 'high'), func_get_args());
-		$low = -1;
-
-		foreach (func_get_args() as $number) {
-			if ($number < $high) {
-				$low = $number;
-			}
-		}
-
-		return $low;
-	}
-
-	/**
 	 * Increase the number to the minimum if below threshold.
 	 *
 	 * @access public
@@ -226,12 +320,42 @@ class Number {
 	 *
 	 * @access public
 	 * @param int $number
+	 * @param int|array $options
 	 * @return string
 	 * @static
 	 */
-	public static function percentage($number) {
-		// @todo - different . and , for locales
-		return number_format($number, 2) . '%';
+	public static function percentage($number, $options = []) {
+		if (is_numeric($options)) {
+			$options = ['places' => $options];
+		}
+
+		$defaults = [
+			'thousands' => ',',
+			'decimals' => '.',
+			'places' => 0
+		];
+
+		// Localization support
+		if (Titon::g11n()->isEnabled()) {
+			$defaults = array_merge($defaults, Titon::g11n()->current()->getFormats('number'));
+		}
+
+		$options = (array) $options + $defaults;
+
+		return number_format(self::precision($number, $options['places']), $options['places'], $options['decimals'], $options['thousands']) . '%';
+	}
+
+	/**
+	 * Formats a number with a level of precision (even if it had none).
+	 *
+	 * @access public
+	 * @param float $number
+	 * @param int $precision
+	 * @return float
+	 * @static
+	 */
+	public static function precision($number, $precision = 2) {
+		return sprintf('%01.' . $precision . 'F', $number);
 	}
 
 	/**
@@ -246,24 +370,12 @@ class Number {
 		if ($number < 0) {
 			return -1;
 
-		} else if ($number === 0) {
+		} else if ($number == 0) {
 			return 0;
 
 		} else {
 			return 1;
 		}
-	}
-
-	/**
-	 * Alias method for toInt().
-	 *
-	 * @access public
-	 * @param int $number
-	 * @return int
-	 * @static
-	 */
-	public static function to($number) {
-		return self::toInt($number);
 	}
 
 	/**
@@ -275,20 +387,8 @@ class Number {
 	 * @return int
 	 * @static
 	 */
-	public static function toBinary($number, $base = 10) {
-		return self::convert($number, $base, 2);
-	}
-
-	/**
-	 * Returns as a byte (or what the PHP integer representation would be).
-	 *
-	 * @access public
-	 * @param int $number
-	 * @return int
-	 * @static
-	 */
-	public static function toByte($number) {
-		return ($number << (64 - 8)) / (1 << (64 - 8));
+	public static function toBinary($number, $base = self::DECIMAL) {
+		return self::convert($number, $base, self::BINARY);
 	}
 
 	/**
@@ -300,20 +400,8 @@ class Number {
 	 * @return int
 	 * @static
 	 */
-	public static function toDecimal($number, $base = 10) {
-		return self::convert($number, $base, 10);
-	}
-
-	/**
-	 * Converts the number to a float.
-	 *
-	 * @access public
-	 * @param int $number
-	 * @return float
-	 * @static
-	 */
-	public static function toFloat($number) {
-		return (float) $number;
+	public static function toDecimal($number, $base = self::DECIMAL) {
+		return self::convert($number, $base, self::DECIMAL);
 	}
 
 	/**
@@ -325,20 +413,8 @@ class Number {
 	 * @return string
 	 * @static
 	 */
-	public static function toHex($number, $base = 10) {
-		return self::convert($number, $base, 16);
-	}
-
-	/**
-	 * Converts the number to an integer.
-	 *
-	 * @access public
-	 * @param int $number
-	 * @return int
-	 * @static
-	 */
-	public static function toInt($number) {
-		return (int) $number;
+	public static function toHex($number, $base = self::DECIMAL) {
+		return self::convert($number, $base, self::HEX);
 	}
 
 	/**
@@ -350,8 +426,8 @@ class Number {
 	 * @return string
 	 * @static
 	 */
-	public static function toOctal($number, $base = 10) {
-		return self::convert($number, $base, 8);
+	public static function toOctal($number, $base = self::DECIMAL) {
+		return self::convert($number, $base, self::OCTAL);
 	}
 
 }
