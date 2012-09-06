ddf909af25c086a0bd9dbf17f5fd5e1d70bf92ad
Miles Johnson <mileswjohnson@gmail.com>
2012-09-05 17:40:37 -0700

    Added classes related to security

---
+++ libs/listeners/security/CsrfProtectionListener.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Titon: A PHP 5.4 Modular Framework
+ *
+ * @copyright	Copyright 2010, Titon
+ * @link		http://github.com/titon
+ * @license		http://opensource.org/licenses/bsd-license.php (BSD License)
+ */
+
+namespace titon\libs\listeners\security;
+
+use titon\Titon;
+use titon\libs\listeners\ListenerAbstract;
+
+/**
+ * @todo
+ *
+ * @package	titon.libs.listeners.security
+ */
+class CsrfProtectionListener extends ListenerAbstract {
+
+}
